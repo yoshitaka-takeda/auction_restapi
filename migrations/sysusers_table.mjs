c5c2b18896282c6cce54b4e26ce5162b0e9253e4
Kristian Amadeus <amadeus.kristian@gmail.com>
2024-01-09 10:12:03 +0700

    2024-01-09__10h11m: [sysusers migration and model(?)]

---
+++ migrations/sysusers_table.mjs
@@ -0,0 +1,32 @@
+import dotenv from '../configs/environment.mjs';
+import orm from '../configs/dbs.mjs';
+
+dotenv.config();
+process.env.SYSUSERS_TABLE = 'sysusers';
+const tableName = process.env.SYSUSERS_TABLE;
+
+let __schema = orm.__knex;
+
+let sysusers_table = {
+    up: async function up(table=tableName){
+        return await __schema.schema.createTable(table, function(t) {
+            t.increments('id').primary();
+            t.string('username',255);
+            t.text('first_name').notNullable();
+            t.text('last_name').notNullable();
+            t.text('bio');
+            // t.text('personal_uuid');
+            t.string('email',255).notNullable();
+            t.text('session_id');
+            t.text('password');
+            t.timestamps(true, __schema.fn.now(8), false);
+        });
+    },
+
+    down: async function down(table=tableName){
+        return await __schema.schema.dropTable(table);
+        
+    }
+}
+
+export default sysusers_table;
---
+++ models/sysusers.mjs
@@ -0,0 +1,25 @@
+import dotenv from '../configs/environment.mjs';
+import orm from '../configs/dbs.mjs';
+import sysusers_table from '../migrations/sysusers_table.mjs';
+import { Model } from 'objection';
+
+dotenv.config();
+process.env.SYSUSERS_TABLE = 'sysusers';
+let exists = orm.__knex;
+
+exists = await exists.schema.hasTable(process.env.SYSUSERS_TABLE).then((exists) => {
+    return exists;
+});
+
+if(!exists){
+    sysusers_table.up(process.env.SYSUSERS_TABLE);
+}
+
+class sysusers extends Model {
+    static get tableName() {
+        return process.env.SYSUSERS_TABLE;
+    }
+
+}
+
+export default new sysusers();
